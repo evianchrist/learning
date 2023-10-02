fe93ef8811c22fd5d694cfe5a8aafb9565242447
flflvm97 <chankim97@outlook.com>
2023-10-02 22:23:47 +0100

    const examples

--- c/constants.c
+++ c/constants.c
@@ -3,9 +3,12 @@
 int main()
 {
     const int myNum = 15;
-    myNum = 10;
 
     printf("%d \n", myNum);
+
+    // some examples of const
+    const int minutesPerHour = 60;
+    const float pi = 3.14;
 
     return 0;
 }
