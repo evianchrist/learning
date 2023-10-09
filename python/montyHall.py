a424a18d946cbd0634353c5aae1022e6343639ea
flflvm97 <chankim97@outlook.com>
2023-10-09 23:14:24 +0100

    change variable and base code for opening goat door

--- python/montyHall.py
+++ python/montyHall.py
@@ -5,6 +5,9 @@ doors = ['goat', 'goat', 'car']
 
 while tries > 0:
     random.shuffle(doors)
-    
+    # chose first door
+    if doors[1] == 'goat': # open the second door and show the goat
+
+    else: # open the third door and show the goat
 
     tries -= 1
