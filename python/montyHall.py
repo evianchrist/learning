9d6b9b68980a9ddf6e51c1a54431cc23793bf57e
flflvm97 <chankim97@outlook.com>
2023-10-09 23:18:48 +0100

    all the scenarios done for second door being goat

--- python/montyHall.py
+++ python/montyHall.py
@@ -2,12 +2,23 @@ import random
 
 tries = 100
 doors = ['goat', 'goat', 'car']
+outcome = [0, 0, 0, 0] # 1. wins after change 2. losses after change 3. wins after not change 4. losses after not change
 
 while tries > 0:
     random.shuffle(doors)
+    change = random.randint(0,1) # change or not to change?
     # chose first door
     if doors[1] == 'goat': # open the second door and show the goat
-
+        if change: # if change the door
+            if doors[2] == 'car':
+                outcome[0] += 1
+            else:
+                outcome[1] += 1
+        else:
+            if doors[0] == 'car':
+                outcome[2] += 1
+            else:
+                outcome[3] += 1
     else: # open the third door and show the goat
 
     tries -= 1
