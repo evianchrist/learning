38ff67095d711f50ec408ec18b9d844cc45cc25b
flflvm97 <chankim97@outlook.com>
2023-10-17 23:45:34 +0100

    Merged Sorted Arrays complete solution

--- python/leetcode/MergeSortedArrays.py
+++ python/leetcode/MergeSortedArrays.py
@@ -3,7 +3,7 @@ class Solution:
         a, b, i = m - 1, n - 1, m + n - 1
 
         while b >= 0:
-            if nums1[a] > nums2[b]:
+            if nums1[a] > nums2[b] and a >= 0:
                 nums1[i] = nums1[a]
                 a -= 1
             else:
