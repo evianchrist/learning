bbc4da73481fc1a90740ed67dffc1124d2e8d75c
flflvm97 <chankim97@outlook.com>
2023-10-30 21:01:18 +0000

    improved version?

--- python/leetcode/RemoveElement.py
+++ python/leetcode/RemoveElement.py
@@ -13,3 +13,14 @@ class Solution:
 
 # runtime 39ms - beats 71.55% of users
 # memory 16.29mb - beats 44.09% of users
+
+
+# refactored ver.
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for n, i in enumerate(nums):
+            if i != val:
+                nums[k] = i
+                k += 1
+        return k
