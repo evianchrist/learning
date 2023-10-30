5fda82305cbcedeceb9555759c40c1a09d6122b1
flflvm97 <chankim97@outlook.com>
2023-10-30 20:57:16 +0000

    remove element problem solved

--- python/leetcode/RemoveElement.py
+++ python/leetcode/RemoveElement.py
@@ -1,2 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for n, i in enumerate(nums):
+            if i == val:
+                nums[n] = -1
+            else:
+                nums[k] = i
+                k += 1
+        return k
