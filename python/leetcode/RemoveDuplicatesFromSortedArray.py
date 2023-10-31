aec62c12bdb96d3c706a51c0b5e6be248b7bc2c1
flflvm97 <chankim97@outlook.com>
2023-10-31 19:54:38 +0000

    solved and passed test

--- python/leetcode/RemoveDuplicatesFromSortedArray.py
+++ python/leetcode/RemoveDuplicatesFromSortedArray.py
@@ -1,3 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        prev = None
+        k = 0
+        for n, i in enumerate(nums):
+            if prev != i:
+                nums[k] = i
+                k += 1
+            prev = i
+        return k
