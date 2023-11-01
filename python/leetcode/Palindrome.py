ed26d3d222754bbee2c006031b8a8f761ef0dfee
flflvm97 <chankim97@outlook.com>
2023-11-01 22:54:53 +0000

    Palindrome solved

--- python/leetcode/Palindrome.py
+++ python/leetcode/Palindrome.py
@@ -1,2 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        output = ""
+        s = s.lower()
+        for i in s:
+            if (48 <= ord(i) <= 57) or (97 <= ord(i) <= 122):
+                output = output + i
+        s = output
+        output = s[::-1]
+
+        if s == output:
+            return True
+        else:
+            return False
+        
