7d58a8d812609feddfa0621b29f1f33593044952
flflvm97 <chankim97@outlook.com>
2023-11-08 23:47:59 +0000

    majortyElement solved

--- python/leetcode/MajorityElements.py
+++ python/leetcode/MajorityElements.py
@@ -1,3 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+        x = len(nums)/2
+
+        for i in nums:
+            if nums.count(i) > x:
+                return i
