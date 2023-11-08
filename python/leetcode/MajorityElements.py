8ef84e71a667286a0c60742e37401277335e8e1c
flflvm97 <chankim97@outlook.com>
2023-11-08 23:51:20 +0000

    majortyElement solved but now takes much less time (it checks duplicates element in the array)

--- python/leetcode/MajorityElements.py
+++ python/leetcode/MajorityElements.py
@@ -1,7 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         x = len(nums)/2
+        rubbish = list()
 
         for i in nums:
-            if nums.count(i) > x:
-                return i
+            if not i in rubbish:
+                if nums.count(i) > x:
+                    return i
+                else:
+                    rubbish.append(i)
