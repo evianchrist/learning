8cff026cf875ba1353241455093a779e0deb8ee3
flflvm97 <chankim97@outlook.com>
2023-11-09 00:10:37 +0000

    isSubsequence solved

--- python/leetcode/InSubsequence.py
+++ python/leetcode/InSubsequence.py
@@ -1,3 +1,19 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
+        k = 0
+        found = 0
+        if len(t) < len(s):
+            return False
+
+        for count, i in enumerate(s):
+            found = 0
+            while found == 0:
+                if i == t[k]:
+                    found = 1
+                k += 1
+                if k >= len(t) and found == 0:
+                    return False
+                if k >= len(t) and count < (len(s)-1):
+                    return False
+        return True
+                
